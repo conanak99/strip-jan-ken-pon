812cd07f48986aa66f97e9f47c99b3601209fd46
Harry Pham <harrypham@swatmobile.io>
2020-06-16 17:32:50 +0800

    Updates

---
+++ precache-manifest.f53a54cd6cb2ae7221e0d66c355fcee4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "709e7b063154262f986d42888785f424",
+    "url": "http://strip.toidicodedao.com/index.html"
+  },
+  {
+    "revision": "bd87175e9e6011544a40",
+    "url": "http://strip.toidicodedao.com/static/css/main.7efd0747.chunk.css"
+  },
+  {
+    "revision": "70c3a8a58c35db0ba0ed",
+    "url": "http://strip.toidicodedao.com/static/js/2.d3567cb1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "http://strip.toidicodedao.com/static/js/2.d3567cb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd87175e9e6011544a40",
+    "url": "http://strip.toidicodedao.com/static/js/main.09ef6a59.chunk.js"
+  },
+  {
+    "revision": "d7e7405160fdf6c11c6e",
+    "url": "http://strip.toidicodedao.com/static/js/runtime-main.3f9e2a49.js"
+  }
+]);
